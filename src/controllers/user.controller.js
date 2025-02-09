543c3c65e788f2d4007209e998f1943e5b9159ce
tunaegilmez <tunaegilmez07@gmail.com>
2025-02-09 16:31:59 +0300

    edit user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
+const { where } = require("sequelize");
 const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const { emailValidator } = require("../helper/validator");
 
 exports.createUser = async (req, res) => {
   try {
@@ -13,6 +16,57 @@ exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll();
     res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.editUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      name,
+      surname,
+      email,
+      username,
+      password,
+      profileImage,
+      age,
+      gender,
+    } = req.body;
+
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (email && !emailValidator(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    let hashedPassword = user.password;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      hashedPassword = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await user.update({
+      name: name || user.name,
+      surname: surname || user.surname,
+      email: email || user.email,
+      username: username || user.username,
+      password: hashedPassword,
+      profileImage: profileImage || user.profileImage,
+      age: age || user.age,
+      gender: gender || user.gender,
+    });
+
+    res.json(updatedUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const {
   createUser,
   getAllUsers,
+  editUser,
   deleteUser,
 } = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -108,6 +110,95 @@ router.get("/", getAllUsers);
  *         description: Geçersiz giriş verisi.
  */
 router.post("/", createUser);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Belirtilen ID'ye sahip kullanıcıyı güncelle
+ *     description: Kullanıcı ID'sine göre kullanıcı bilgilerini günceller.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Güncellenecek kullanıcının ID'si
+ *         example: 1
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Ahmet"
+ *               surname:
+ *                 type: string
+ *                 example: "Yılmaz"
+ *               email:
+ *                 type: string
+ *                 example: "ahmetyeni@example.com"
+ *               username:
+ *                 type: string
+ *                 example: "ahmetyeni"
+ *               password:
+ *                 type: string
+ *                 example: "yenişifre123"
+ *               profileImage:
+ *                 type: string
+ *                 example: "https://example.com/new-profile.jpg"
+ *                 nullable: true
+ *               age:
+ *                 type: string
+ *                 example: "30"
+ *               gender:
+ *                 type: string
+ *                 enum: ["male", "female", "-"]
+ *                 example: "male"
+ *     responses:
+ *       200:
+ *         description: Kullanıcı başarıyla güncellendi.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: "Ahmet"
+ *                 surname:
+ *                   type: string
+ *                   example: "Yılmaz"
+ *                 email:
+ *                   type: string
+ *                   example: "ahmetyeni@example.com"
+ *                 username:
+ *                   type: string
+ *                   example: "ahmetyeni"
+ *                 profileImage:
+ *                   type: string
+ *                   example: "https://example.com/new-profile.jpg"
+ *                 age:
+ *                   type: string
+ *                   example: "30"
+ *                 gender:
+ *                   type: string
+ *                   enum: ["male", "female", "-"]
+ *                   example: "male"
+ *       400:
+ *         description: Geçersiz giriş verisi.
+ *       404:
+ *         description: Kullanıcı bulunamadı.
+ */
+router.put("/:id", authMiddleware, editUser);
 
 /**
  * @swagger
